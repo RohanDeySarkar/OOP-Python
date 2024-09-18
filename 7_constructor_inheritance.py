a9d15e75e6e8050685d0215275fc7c330761b296
RohanDeySarkar <41055662+RohanDeySarkar@users.noreply.github.com>
2024-09-18 22:09:25 +0530

    Update 7_constructor_inheritance.py

--- 7_constructor_inheritance.py
+++ 7_constructor_inheritance.py
@@ -36,3 +36,25 @@ class B(A):
 # If no innit in B, takes A'init
 # For getting noth A/B init in B, use seuper in B init
 b1 = B()
+
+####################################################################################################
+# Parent class
+class A:
+   def __init__(self, a_name):
+       self.a_name = a_name
+   
+# Child class
+class B(A):
+    def __init__(self, b_name, a_name):
+        self.b_name = b_name
+        # invoke constructor of class A
+        A.__init__(self, a_name)
+    
+    def display_names(self):
+        print("A name : ", self.a_name)
+        print("B name : ", self.b_name)
+
+
+#  Driver code
+b = B('child', 'parent')
+b.display_names()
